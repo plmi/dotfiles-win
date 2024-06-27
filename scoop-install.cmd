d24ca8bfd73d1aa97908d545b23389de6fafd1a5
plmi <65455617+plmi@users.noreply.github.com>
2024-06-27 10:19:36 +0200

    Update scoop-install.cmd

--- scoop-install.cmd
+++ scoop-install.cmd
@@ -1,2 +1,4 @@
 scoop bucket add extras
-scoop install main/7zip extras/anki main/aria2c extras/autohotkey extras/everything main/fastfetch main/git main/gpg extras/komorebi extras/mpv main/msys2 main/neovim extras/notepadplusplus extras/pass-winmenu-nogpg extras/powertoys extras/qbittorrent extras/signal extras/ungoogled-chromium extras/wezterm extras/whkd extras/windows-terminal main/zoxide main/fzf extras/psfzf scoop install main/pwsh
+scoop install main/git
+:: scoop install extras/autohotkey extras/komorebi main/msys2 extras/whkd
+scoop install main/7zip extras/anki main/aria2 extras/everything main/fastfetch main/gpg extras/mpv main/neovim extras/notepadplusplus extras/pass-winmenu-nogpg extras/powertoys extras/qbittorrent extras/signal extras/chromium extras/thunderbird extras/windows-terminal main/zoxide main/fzf extras/psfzf main/pwsh main/rclone main/syncthing extras/vcredist2022
